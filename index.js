56bc5bbad117ccd367445b8c0acc721ef61a80b0
EkoT <75649447+iIEkoT@users.noreply.github.com>
2021-11-01 01:17:48 +0200

    updating api to 4.0 beta

--- index.js
+++ index.js
@@ -1,103 +1,114 @@
 require("dotenv").config()
-const { Client } = require("discord.js");
+let fetch = require("node-fetch")
+const { Client, Message, MessageEmbed } = require("discord.js");
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS", "GUILD_WEBHOOKS", "GUILD_PRESENCES", "GUILD_INVITES", "GUILD_EMOJIS_AND_STICKERS", "GUILD_BANS", "DIRECT_MESSAGE_TYPING", "DIRECT_MESSAGE_REACTIONS", "DIRECT_MESSAGES"]});
 
 client.on("ready", () => {
     console.log(`connected with ${client.user.tag}!`);
     let channel = client.channels.cache.get("893849568800808960");
-    channel.send("> :white_check_mark: API connected `v3`")
+    //channel.send("> :white_check_mark: API connected `Developer mode`")
 })
 
-function API(request, send, fetch, set, add, remove) {
-/**
-@const { API }[API]:[moduleRequest]Required!Important;
-@var { API }[Request]:[excute]Required;
-@var { API }[Send]:[excute]Required;
-@var { API }[Fetch]:[excute]Option;
-@var { API }[Set]:[export]Option;
-@var { API }[Add]:[async]Option;
-@var { API }[Remove]:[async]Option;
-*/
-HttpsRequest = "https://hino.gq/api"["startsWith".replace("http://hino.gq/api")]
+if(client.ws.ping == NaN) {
+    client.ws.ping = "Cannot get latency"
 }
 
-API.addCommand = async function(commands, create, remove) { 
-
-API.addCommand = client.options({API: true, command: true, create: true})
+let API = {
+    name: "Hino_API",
+    URL: "https://hino.gq/api",
+    version: "3.3",
+    latency: client.ws.ping,
+    shards_count: "2",
+    shards: {
+        "shard1": {
+            "name": "Shard 1",
+            "version": "1.0",
+            "users": "soon",
+            "servers": "soon",
+            "latency": `${client.ws.ping * Date.now()}ms`,
+            "type": "Default shard"
+        },
+        "shard2": {
+            "name": "Shard 2",
+            "version": "1.0",
+            "users": "soon",
+            "servers": "soon",
+            "latency": `${client.ws.ping}ms`,
+            "type": "Helper shard"
+        }
+    },
+    "error": {
+        "device": {
+            "unsupported": "Your device are unsupported to use Hino's API",
+            "noBase": "You don't have an much base to use Hino's API, required base: 6MB"
+        },
+        "node": {
+            "unsupportedNode": "Unsupported node version, the required node is: 16 or higher"
+        },
+        "handler": {
+            "unsupportedHandler": "Unsupported handler, (required handler is: \"node-fetch\")"
+        },
+        "status": {
+            "errorCodeStatus": "$ echo err status code R?{statusCode.displayStatus}D $$ err"
+        },
+        "APIerrors": {
+            "unsupportedVersion": "unsupported API version, required version is: 3.0 or higher"
+        }, 
+        "disconnectErrors": {
+            "APIdisconnected": "API disconnected, $ echo err api disconnected R?{API.disconnectMessage}D $$ err",
+            "websocketDisconnected": "WebSocket disconnected, $ echo err websocket disconnected R?{API.websocket.status('DISCONNECTED').sendMessage()}D $$ err",
+            "handler Disconnected": "$ echo err handler disconnected R?{API.fetch('handler').name || API.handler.name || API.handler.undefined}D $$ disconnected"
+        },
+        "clientErrors": {
+            "undefinedGuild": "This guild are undefined by local",
+            "undefinedUser": "This user are undefined by local",
+            "notDeveloper": "You are not developer",
+            "undefinedDeveloper": "This developer are undefined by client catcher"
+        }
+     } 
 }
 
-API.removeCommand = async function(commands, create, remove) { 
-
-    API.removeCommand = client.options({API: true, command: true, remove: true})
-    }
-
-    API.intents = {
-            CREATED_COMMANDS_FETCH: async slash => {
-                
-            API.addCommand({Guildonly: false, public: true}, { 
-                name: "FetchGuild",
-                description: "API intent"
-            })
-            },
-            FETCH_GUILDS: client.guilds.cache.map(g => {
-                g.id
-            }),
-            FETCH_USERS: client.users.cache.map(u => {
-                u.tag
-            }),
-            GET_USER_ID: client.users.cache.map(user => {
-                user.id
-            }),
-            REQUEST: new API(request => {
-                request.fetch(API) >[module.filename]> request.apt(APII.clientIntents) >[module.id]> request.intent();
-            })
-
-    }
-
-
-let APII = {
-    apiUrl: "https://hino.gq/api",
-    clientEvent: client,
-    clientIntents: [
-    "GUILDS",
-    "GUILD_MESSAGES",
-    "GUILD_MEMBERS",
-    "GUILD_WEBHOOKS",
-    "GUILD_PRESENCES",
-    "GUILD_INVITES",
-    "GUILD_EMOJIS_AND_STICKERS",
-    "GUILD_BANS",
-    "DIRECT_MESSAGE_TYPING",
-    "DIRECT_MESSAGE_REACTIONS",
-    "DIRECT_MESSAGES"],
-}
-
-client.on("messageCreate", message => {
-    if(message.content.startsWith("l.aintents")) {
-        message.channel.send(`API intents: ${API.intents}\n`)
-        console.log(API.intents)
-    }
-})
 
 client.login(process.env.TOKEN);
 
-new Event(API, Array.from(String(API), os => {
-    void API, os >> toString(void API.intents)>> APII.clientEvent, static => {
-        static(APII.clientEvent)
-    }
-}))
-
-new Event(API.intents["REQUEST"], request => {
-    request.fetch(client.requests);
-    fetch(request, API.intents["REQUEST"])
-})
-
 let base = require("./base.json");
 
-fetch(base.options, base.optionscount, base.requiredOptions).catch(err => {
-    console.log(base.errors, err)
+client.on("messageCreate", message => {
+    if(message.content.startsWith("lu.api")) {
+        let embed = new MessageEmbed()
+        .setAuthor("API info", client.user.displayAvatarURL({ dynamic: true }))
+        .addFields({
+name: "> API",
+value: `\`\`\`js
+name: ${API.name}
+version: ${API.version}
+URL: ${API.URL}
+Latency: ${client.ws.ping}
+Shards: ${API.shards_count}
+\`\`\``
+        }, {
+            name: "Shard 1",
+            value: `\`\`\`js
+Version: ${API.shards.shard1["version"]},
+Type: ${API.shards.shard1["type"]}
+            \`\`\``,
+            inline: true
+        }, {
+            name: "Shard 2",
+            value: `\`\`\`js
+Version: ${API.shards.shard2["version"]},
+Type: ${API.shards.shard2["type"]}
+            \`\`\``,
+            inline: true
+        })
+        .setColor("#f2af66")
+        .setImage("https://i.imgur.com/wCicMxx.png")
+        message.reply({embeds: [embed]})
+    }
 })
 
-base.requests({
-    "REQUESTED": "MODULE"
+client.on("messageCreate", message => {
+    if(message.content.startsWith("lu.ping")) {
+        message.reply({content: `${client.ws.ping}`})
+    }
 })
