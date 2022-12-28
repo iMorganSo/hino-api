b32d9722c035ec1efec77cf837862d876ec27c9f
EkoT <morjan.mj11@gmail.com>
2022-12-29 00:52:08 +0200

    typescript update

---
+++ index.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+const API = {
+    version: "v1.0",
+    url: "https://hino.tk/api",
+    connect: function(url = "https://hino.tk/api") {
+    axios.get(url).then(res => {
+         console.log(res.data);
+    })
+    },
+    status: function(url = "https://hino.tk/api") {
+        axios.get(url).then(res => {
+             console.log(res.status);
+        })
+    },
+    latency: function(url = "https://hino.tk/api") {
+    const start = Date.now()
+    axios.get(url)
+    const finish = Date.now()
+    const time = (finish - start) / 1000;
+     console.log(time);
+    }
+};
+
+API;
